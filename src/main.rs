f9d5996aa10eb42b46807d145b8d56ec2293343c
xorphitus <xorphitus@gmail.com>
2019-02-22 19:22:28 +0900

    get mp3 track number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use id3::{Tag};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} have to specify a mp3 file name", &args[0]);
+        return;
+    }
+    let filename = &args[1];
+    let tag = Tag::read_from_path(filename).unwrap();
+    println!("{}", tag.track().unwrap());
 }
